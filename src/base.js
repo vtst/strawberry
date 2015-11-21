e5701a2ad28c620680be646d2bbac8e685be547f
vtst <github.self@vtst.net>
2015-11-21 18:27:41 +0100

    DialogFactory

--- src/base.js
+++ src/base.js
@@ -95,81 +95,135 @@ swby.base.EventHandler.prototype.listen = function(target, eventTypes, handler, 
 };
 
 
-// ****************************************************************************
-// Class swby.base.ErrorReporter
+// *************************************************************************
+// Interface sbwy.base.DialogFactory
+
+/**
+@interface
+*/
+swby.base.DialogFactory = function() {};
+
+/**
+@param {{title: string, message: string, ok_label: string, cls: string}}
+@param {Function} callback
+*/
+swby.base.DialogFactory.prototype.showDialog = function(params, callback) {};
+
+
+// -------------------------------------------------------------------------
+// Class swby.base.AlertDialogFactory
+
+/**
+@implements {swby.base.DialogFactory}
+*/
+swby.base.AlertDialogFactory = function() {};
+
+swby.base.AlertDialogFactory.prototype.showDialog = function(
+  params, callback) {
+  alert(params.title + 
+        (params.message ? '\n\n' + params.message : '') +
+        (params.details ? '\n\n' + params.details : ''));
+  callback();
+};
+
+
+// -------------------------------------------------------------------------
+// Class swby.base.BootstrapDialogFactory
+
+/**
+@implements {swby.base.DialogFactory}
+*/
+swby.base.BootstrapDialogFactory = function() {};
 
 /** @private {string} */
-swby.base.ERROR_DIALOG_HTML_ = '\
+swby.base.BootstrapDialogFactory.DIALOG_HTML_ = '\
   <div class="modal fade" id="my-modal">\
     <div class="modal-dialog">\
       <div class="modal-content">\
         <div class="modal-header">\
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>\
-          <h4 class="modal-title text-danger lead">Oops! Something went wrong</h4>\
+          <h4 class="modal-title lead"></h4>\
         </div>\
-        <div class="modal-body">\
-          <div class="error-reporter-message lead"></div>\
-          <div class="error-reporter-details" style="white-space: pre-wrap;"></div>\
-        </div>\
+        <div class="modal-body"></div>\
         <div class="modal-footer">\
-          <button type="button" class="btn btn-danger" data-dismiss="modal">Continue</button>\
+          <button type="button" class="btn" data-dismiss="modal">\
+          </button>\
         </div>\
       </div>\
     </div>\
   </div>';
 
-// TODO: Max height
-/**
-@param {string} message
-@param {string|Object} details
-@param {boolean=} opt_fatal
- */
-swby.base.showErrorDialog = function(message, details, opt_fatal) {
-  function setTextContent(element, textContent) {
-    if (textContent) element.textContent = textContent;
-    else element.style.display = 'none';
+swby.base.BootstrapDialogFactory.prototype.showDialog = function(
+  params, callback) {
+  var cls = params.cls || 'primary';
+  var dialog = swby.util.renderHtmlAsElement(
+    swby.base.BootstrapDialogFactory.DIALOG_HTML_);
+  var title = swby.util.getElementByClassName(dialog, 'modal-title');
+  title.classList.add('text-' + cls);
+  title.textContent = params.title;
+  var body = swby.util.getElementByClassName(dialog, 'modal-body');
+  if (params.message) {
+    var p = document.createElement('p');
+    p.textContent = params.message;
+    body.appendChild(p);
   }
-  function formatDetails(status, statusText) {
-    return 'Error ' + status + (statusText ? ': ' + statusText : '');
+  if (params.details) {
+    var p = document.createElement('p');
+    p.textContent = params.message;
+    p.style = 'white-space: pre-wrap; font-size: 80%; font-family: monospace;';
+    body.appendChild(p);
   }
-  
-  var dialog = swby.util.renderHtmlAsElement(swby.base.ERROR_DIALOG_HTML_);
+  if (params.ok_label) {
+    var btn = swby.util.getElementByClassName(dialog, 'btn');
+    btn.classList.add('btn-' + cls);
+    btn.textContent = params.ok_label;
+  } else {
+    var footer = swby.util.getElementByClassName(dialog, 'modal-footer');
+    footer.style.display = 'none';
+  }
   document.body.appendChild(dialog);
-  setTextContent(swby.util.getElementByClassName(dialog, 'error-reporter-message'), message);
-  if (typeof(details) == 'object') {
-    if (details.error && details.error.code) details = formatDetails(details.error.code, details.error.message);
-    else if (details.result && details.result.error && details.result.error.code)
-      details = formatDetails(details.result.error.code, details.result.error.message || details.statusText);
-    else if (details.status && details.statusText) details = formatDetails(details.status, details.statusText);
-    else details = null;
-  }
-  setTextContent(swby.util.getElementByClassName(dialog, 'error-reporter-details'), details);
-  if (opt_fatal) {
-    swby.util.getElementByClassName(dialog, 'modal-footer').style.display = 'none';
-    swby.util.getElementByClassName(dialog, 'close').style.display = 'none';
-  }
   $(dialog).modal('show');
   $(dialog).on('hidden.bs.modal', function(event) {
     document.body.removeChild(dialog);
+    callback();
   });
 };
 
-/**
-@param {string} message
-@param {boolean} fatal
-@param {string|Object} details
-@private
- */
-swby.base.reportApiError_ = function(message, fatal, details) {
-  swby.base.showErrorDialog(message, details, fatal);
-};
 
+// ****************************************************************************
+// Class swby.base.ErrorReporter
+
+// TODO: Max height
 /**
-@param {string=} opt_message
+@param {sbwy.base.DialogFactory} factory
+@param {string} message
+@param {string|Object} details
 @param {boolean=} opt_fatal
  */
-swby.base.apiErrorHandler = function(opt_message, opt_fatal) {
-  return swby.base.reportApiError_.bind(null, opt_message || 'An API call failed', opt_fatal || false);
+swby.base.showErrorDialog_ = function(factory, message, details, opt_fatal) {
+  function formatDetails(status, statusText) {
+    return 'Error ' + status + (statusText ? ': ' + statusText : '');
+  }
+  if (typeof(details) == 'object') {
+    if (details.error && details.error.code) {
+      details = formatDetails(details.error.code, details.error.message);
+    } else if (details.result && details.result.error &&
+               details.result.error.code) {
+      details = formatDetails(
+        details.result.error.code,
+        details.result.error.message || details.statusText);
+    } else if (details.status && details.statusText) {
+      details = formatDetails(details.status, details.statusText);
+    } else {
+      details = null;
+    }
+  }
+  factory.showDialog({
+    title: 'Ooops! Something went wrong.',
+    message: message,
+    details: details,
+    ok_label: (opt_fatal ? null : 'Continue'),
+    cls: 'danger'
+  }, function() { if (opt_fatal) document.body.innerHTML = ''; });
 };
 
 // ****************************************************************************
@@ -243,10 +297,12 @@ swby.base.Config;
 // * finalize_ init the page when it has been called three times.
 
 /**
+ @param {*} config
  @param {Function} page_class
+ @param {swby.base.DialogFactory} opt_dialogFactory
  @constructor
  */
-swby.base.Loader = function(config, page_class) {
+swby.base.Loader = function(config, page_class, opt_dialogFactory) {
   swby.base.EventHandler.call(this);
   /** @private {swby.base.Config} */
   this.config_ = config;
@@ -254,7 +310,9 @@ swby.base.Loader = function(config, page_class) {
   this.page_class_ = page_class;
   /** @private {swby.base.Page} */
   this.page_ = null;
-  
+  /** @private {sbwy.base.DialogFactory} */
+  this.dialogFactory_ = (
+    opt_dialogFactory || new swby.base.BootstrapDialogFactory);
   // Start the show.
   swby.promise.all([
     swby.promise.onDomEvent(document, 'DOMContentLoaded').then(function(event) {
@@ -291,31 +349,13 @@ swby.base.Loader.prototype.token_lifetime_s_ = 50 * 60;
 /** @private {number} */
 swby.base.Loader.prototype.token_refresh_interval_ms_ = 45 * 60 * 1000;
 
-/** @private {string} */
-swby.base.Loader.AUTH_DIALOG_HTML_ = '\
-  <div class="modal fade" id="my-modal">\
-    <div class="modal-dialog">\
-      <div class="modal-content">\
-        <div class="modal-header">\
-          <h4 class="modal-title">Authorization needed</h4>\
-        </div>\
-        <div class="modal-body">\
-          <p>This application requires authorization to access some Google APIs. Please click on "Proceed" to run through the authorization process.\
-          This will open a popup window from Google where you will be to review the access rights and approve them.</p>\
-        </div>\
-        <div class="modal-footer">\
-          <button type="button" class="btn btn-primary" data-dismiss="modal">Proceed</button>\
-        </div>\
-      </div>\
-    </div>\
-  </div>';
-
 /**
  @param {*} reason
  @private
  */
 swby.base.Loader.prototype.reportError_ = function(reason) {
-  swby.base.showErrorDialog(reason.message, reason.details, true);
+  swby.base.showErrorDialog_(
+    this.dialogFactory_, reason.message, reason.details, true);
 };
 
 /**
@@ -422,13 +462,15 @@ swby.base.Loader.prototype.authorizeLocally_ = function() {
  @private
  */
 swby.base.Loader.prototype.showAuthDialog_ = function(callback) {
-  var dialog = swby.util.renderHtmlAsElement(swby.base.Loader.AUTH_DIALOG_HTML_);
-  document.body.appendChild(dialog);
-  $(dialog).modal('show');
-  $(dialog).on('hidden.bs.modal', function(event) {
-    document.body.removeChild(dialog);
-    callback();
-  });
+  this.dialogFactory_.showDialog({
+    title: 'Authorization needed',
+    message: ('This application requires authorization to access some Google' +
+              'APIs. Please click on "Proceed" to run through the' +
+              'authorization process. This will open a popup window from ' +
+              'Google where you will be to review the access rights and ' +
+              'approve them.'),
+    ok_label: 'Proceed'
+  }, callback);
 };
 
 /**
